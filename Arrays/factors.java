e623aa732caf81e76c7c1328b9ee7b79d728709b
Shivam mishra <114843423+Shiv259@users.noreply.github.com>
2025-06-18 11:58:56 +0530

    [shivam mishra add. factors.java added]

--- Arrays/factors.java
+++ Arrays/factors.java
@@ -5,10 +5,16 @@ public class factors {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int fact =0;
-        for(int i=1;i<n;i++) 
+        for(int i=1;i<=n;i++) 
         if(n%i==0) fact++;
 
         int arr[] = new int[fact];
-        int maxFact =10;
+        
+		int j = 0;
+		for(int i = 1; i <= n; i++) if(n % i == 0) {
+			arr[j++] = i;
+			System.out.print(i + " ");
+		}
+
     }
 }
