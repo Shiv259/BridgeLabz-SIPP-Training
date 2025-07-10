9191eb9686c0835c1b02a4989372d241c366e6f8
Shivam mishra <114843423+Shiv259@users.noreply.github.com>
2025-07-10 19:29:04 +0530

    oops

---
+++ BankAccountSystem.java
@@ -0,0 +1,30 @@
+public class BankAccountSystem {
+    static String bankName = "BOB Bank";
+	static int totalAccounts = 0;
+	String accountHolderName;
+	final int accountNumber;
+	
+	static void getTotalAccounts() {
+		System.out.println("Total accounts: "+ totalAccounts);
+	}
+	public BankAccountSystem(String accountHolderName, int accountNumber) {
+		this.accountHolderName = accountHolderName;
+		this.accountNumber = accountNumber;
+		totalAccounts++;
+	}
+	
+	public void displayAccountdetails() {
+		System.out.println(accountHolderName + " " + accountNumber + " " + bankName);
+	}
+	
+	public static void main(String[] args) {
+		BankAccountSystem account1 = new BankAccountSystem("Shivam", 99654121);
+		if(account1 instanceof BankAccountSystem) {
+			account1.displayAccountdetails();
+		}
+		
+		
+		
+
+	}
+}
