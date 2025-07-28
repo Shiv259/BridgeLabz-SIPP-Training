5588895e538f9d07392beb4fd3022efa79a789ee
Shivam mishra <114843423+Shiv259@users.noreply.github.com>
2025-07-28 08:47:13 +0530

    stock_span added

---
+++ Stack_Queue/stock_span.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class stock_span {
+	public static void main(String[] args) {
+		int[] arr = {30,35,40,38,35};
+		StockSpan(arr);	
+	}
+	public static void StockSpan(int[] arr) {
+		Stack<Integer> st = new Stack<>();
+		int[] ans = new int[arr.length];
+		for(int i=0; i<arr.length; i++) {
+			while(!st.isEmpty() && arr[i]>=arr[st.peek()]) {
+				st.pop();
+			}
+			if(st.isEmpty()) {
+				ans[i]=i+1;
+			}
+			else {
+				ans[i]=i-st.peek();
+			}
+			st.push(i);
+		}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(ans[i]+" ");
+		}
+	}
+}
