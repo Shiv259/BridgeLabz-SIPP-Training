9eb0b7c786c46001f706ac7a537c706d53fc3af7
Shivam mishra <114843423+Shiv259@users.noreply.github.com>
2025-08-03 23:11:13 +0530

    remove duplicates added

---
+++ Searching/Remove_Duplicates.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Remove_Duplicates {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		HashSet<Character> set = new HashSet<Character>();
+		String str = "abcdabbbcd";
+		for(char ch : str.toCharArray()) {
+			if(!set.contains(ch)) {
+				sb.append(ch);
+			}
+			set.add(ch);
+		}
+		System.out.println(sb.toString());
+	}
+}
